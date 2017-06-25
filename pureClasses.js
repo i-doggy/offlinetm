0e27ce847fcb41d7d36cd07a373d3486166b2649
i-doggy <kojlambus@gmail.com>
2017-06-26 01:46:11 +0500

    more readable code in pureClasses(more structured too)

--- pureClasses.js
+++ pureClasses.js
@@ -4,22 +4,31 @@
 	
 	this.onchange = [];
 	
-	Object.defineProperty(this, 'done', { set: setBoolProp(this, '_isDone', 'onchange'), get: getProp(this, '_isDone')});
+	var setIsDone = setBoolProp(this, '_isDone', 'onchange');
+	Object.defineProperty(this, 'done', { set: setIsDone, get: getProp(this, '_isDone')});
 	this._isDone = false;
-	setBoolProp(this, '_isDone', 'onchange')(done);
+	setIsDone(done);
 	
 	this.onstateChange = [];
-	Object.defineProperty(this, 'isAdditional', { set: setBoolProp(this, '_isAdditional', 'onstateChange'), get: getProp(this, '_isAdditional')});
-	this.isAdditional = false;
-	if(isAdditional) this.isAdditional = true;
 	
-	Object.defineProperty(this, 'progress', { get: function() { if(this.isAdditional) return 0; else return Number(this.done); }});
+	var setIsAdditional = setBoolProp(this, '_isAdditional', 'onstateChange');
+	Object.defineProperty(this, 'isAdditional', { set: setIsAdditional, get: getProp(this, '_isAdditional')});
+	this.isAdditional = false;
+	setIsAdditional(isAdditional);
+	
+	var getProgress = function() {
+		if(this.isAdditional) return 0;
+		else return Number(this.done);
+	}
+	
+	Object.defineProperty(this, 'progress', { get: getProgress });
 	return this;
 }
 
 Task.prototype.toggleDone = function() {
 	if(this.done) this.done = false;
 	else this.done = true;
+	return this.done;
 };
 
 function TaskGroup(name) {
@@ -31,52 +40,84 @@ function TaskGroup(name) {
 	
 	Object.defineProperty(this, 'progress', { get: this.getProgress });
 	Object.defineProperty(this, 'done', { get: this.getDone });
-	Object.defineProperty(this, 'length', { get: function() { return this.child.length + this.child.reduce(function(p, e) { if(e.length) return p + e.length - 1; else return p; }, 0); }});
+	
+	var getLength = function() { //should not return tasks marked as additional
+		return this.child.reduce(function(p, e) { 
+					if(e.length !== undefined) return p + e.length; 
+					else {
+						if(!e.isAdditional)return ++p;
+						else return p;
+					}
+				}, 0); 
+			};
+	
+	Object.defineProperty(this, 'length', { get: getLength });
 	
 	this.additionalLength = 0;
 	this.child = [].slice.call(arguments, 1).map(this.addTask, this);
 }
 
 TaskGroup.prototype.addTask = function (t) {
-		t.onchange.push(this._ChangeHandler(this));
-		t.onstateChange.push(this._StateChangeHandler(this));
-		if(t.isAdditional) this.additionalLength++
-		if(t.additionalLength) this.additionalLength = this.additionalLength ? this.additionalLength - 1 : 0 + t.additionalLength;
+		//Add event listeners
+		t.onchange.push(this._ChangeHandler);
+		t.onstateChange.push(this._StateChangeHandler);
+		//Manage additional length
+		if(t.isAdditional) this.additionalLength++;
+		if(t.additionalLength) this.additionalLength = this.additionalLength + t.additionalLength;
+		//Return added
 		return t;
 }
-TaskGroup.prototype.getProgress = function() { 
+TaskGroup.prototype.getProgress = function() { //returns number of done not additional tasks in group
 	return this.child.reduce(function(p, e) { 
 		return p + e.progress;
 	}, 0);
 }
-TaskGroup.prototype.getDone = function() { 
-	return this.progress == this.length - this.additionalLength; 
+TaskGroup.prototype.getDone = function() { //return true if all not additional tasks is done
+	return this.progress == this.length; 
 }
-TaskGroup.prototype._ChangeHandler = function(obj) { return function(i) { obj.onchange.forEach(function(e) { e(i); }); }};
-TaskGroup.prototype._StateChangeHandler = function(obj) { return function(state) { 
-	if(state) obj.additionalLength++;
-	else obj.additionalLength--;
-	obj.onstateChange.forEach(function(e) { e(state); });
-}};
-TaskGroup.prototype.deleteTask = function(index) {
-	if(!index.inRange(-(this.child.length), this.child.length)) throw Error('Out of range');
-	findItemAndDelete(this.child[index].onchange, this._ChangeHandler(this));
-	findItemAndDelete(this.child[index].onstateChange, this._StateChangeHandler(this));
+TaskGroup.prototype._ChangeHandler = function(value) { //only bubbles event
+	fireEvent(this, 'onchange', value);
+};
+TaskGroup.prototype._StateChangeHandler = function(state) {
+	//Manage additional length
+	if(state) this.additionalLength++;
+	else this.additionalLength--;
+	//Bubble this event
+	fireEvent(this, 'onstateChange', state);
+};
+TaskGroup.prototype._deleteObjecto = function(task, index) { //underscore deleting method
+	//Manage additional length
+	if(task.isAdditional) this.additionalLength--;
+	if(task.additionalLength) this.additionalLength = this.additionalLength - task.additionalLength;
+	//Remove event listeners
+	findItemAndDelete(task.onchange, this._ChangeHandler);
+	findItemAndDelete(task.onstateChange, this._StateChangeHandler);
+	//Return deleted
 	return this.child.splice(index, 1)[0];
+}
+TaskGroup.prototype.deleteTaskByIndex = function(index) {
+	if(!index.inRange(-(this.child.length), this.child.length)) throw Error('Out of range');
+	
+	return this._deleteObjecto(this.child[index], index);
 }
 TaskGroup.prototype.deleteTaskObj = function(task) {
 	var i = this.child.findIndex(function(e) { return e == task; });
-	this.deleteTask(i);
+	if(i != -1) return this._deleteObjecto(task, i);
+	else return false;
 }
 
 function setBoolProp(obj, propname, eventName) {
 	return function(value) {
-		var b = obj[propname] != Boolean(value)
+		var changed = obj[propname] != Boolean(value);
 		obj[propname] = Boolean(value);
 		if(eventName)
-			if(b) 
-				obj[eventName].forEach(function(e) { e(Boolean(value)); });
+			if(changed)
+				fireEvent(obj, eventName, Boolean(value));
 	};
+}
+
+function fireEvent(obj, eventName, value) {
+	obj[eventName].forEach(function(handler) { handler(value); });
 }
 
 function getProp(obj, propname) {
@@ -86,7 +127,9 @@ function getProp(obj, propname) {
 };
 
 function findItemAndDelete(array, item) {
-	array.splice(array.findIndex(function(e) { return e == item; }), 1);
+	var i = array.findIndex(function(e) { return e == item; })
+	if(i != -1) return array.splice(i, 1)[0];
+	else return false;
 }
 
 Number.prototype.inRange = function(start, end) {
--- script.js
+++ script.js
@@ -1,48 +1,34 @@
-﻿//CHERNOVIK
-function measureTotal(form) {
-	return form.length;
-}
-function measureChecked(form) {
-	var total = [].reduce.call(form, function(p, e) { return p + e.checked; }, 0);
-	return total
-}
+﻿function RealTask(name, isAdditional, isDone) {
+	Task.apply(this, [].slice.call(arguments));
+	
+	var el = document.createElement('LI')
+	
+	//<div class='checkbox'><span>&#x2713;<span></div>
+	var checkbox = document.createElement('DIV');
+	checkbox.classList.add('checkbox');
+	var check = document.createElement('SPAN');
+	check.innerText = '✓'
+	checkbox.append(check);
+	el.append(checkbox);
+	
+	var span = document.createElement('SPAN');
+	span.innerText = name;
+	span.contentEditable = "true";
+	el.append(span);
+	
+	//<div class='deletepoint'>&#x274C;</div>
+	var deletebox = document.createElement('DIV');
+	deletebox.innerText = '❌';
+	deletebox.classList.add('deletebox');
+	el.append(deletebox);
+	
+	el.task = this;
+	this.element = el;
 
-function RealTask(name, isAdditional, isDone, elem) {
-	Task.apply(this, [].slice.call(arguments, 0, 3));
-	if(elem) {
-		if(elem.tagName.toUpperCase() == 'LI') {
-			if(elem.children.length == 0)
-				this.element = elem;
-			else throw Error('Syntax Error: elem has children');
-		} else throw Error('Syntax Error: elem is not li element');
-	} else {
-		var el = document.createElement('LI')
-		
-		//<div class='checkbox'><span>&#x2713;<span></div>
-		var checkbox = document.createElement('DIV');
-		checkbox.classList.add('checkbox');
-		var check = document.createElement('SPAN');
-		check.innerText = '✓'
-		checkbox.append(check);
-		el.append(checkbox);
-		
-		var span = document.createElement('SPAN');
-		span.innerText = name;
-		span.contentEditable = "true";
-		el.append(span);
-		
-		//<div class='deletepoint'>&#x274C;</div>
-		var deletebox = document.createElement('DIV');
-		deletebox.innerText = '❌';
-		deletebox.classList.add('deletebox');
-		el.append(deletebox);
-		
-		el.task = this;
-		this.element = el;
-	}
 	if(this.isAdditional) this.element.classList.add('additional');
 	this.element.classList.add('task');
 	if(this.done) this.element.classList.add('checked');
+	
 	return this;
 }
 
@@ -81,11 +67,21 @@ RealTaskGroup.prototype._createElement = function(isList) {
 	this.element = container;
 }
 
-RealTaskGroup.prototype._ChangeHandler = function(obj) { 
-	return function(i) { 
-		obj.renderProgress();
-		obj.onchange.forEach(function(e) { e(i); }); 
-	}
+RealTaskGroup.prototype.addTask = function (t) {
+		//Add event listeners
+		t.onchange.push(this._ChangeHandler.bind(this));
+		t.onstateChange.push(this._StateChangeHandler);
+		//Manage additional length
+		if(t.isAdditional) this.additionalLength++;
+		if(t.additionalLength) this.additionalLength = this.additionalLength + t.additionalLength;
+		//Return added
+		return t;
+}
+RealTaskGroup.prototype._ChangeHandler = function(value) {
+	console.log(this);
+	this.renderProgress();
+	TaskGroup.prototype._ChangeHandler.call(this, value);
+	//obj.onchange.forEach(function(e) { e(i); }); 
 };
 RealTaskGroup.prototype.deleteTaskObj = function(task) {
 	var res = TaskGroup.prototype.deleteTaskObj.call(this, task);
@@ -93,8 +89,8 @@ RealTaskGroup.prototype.deleteTaskObj = function(task) {
 	return res;
 }
 RealTaskGroup.prototype.renderProgress = function() { 
-	this.progressElement.max = this.length - this.additionalLength;
-	if(!(this.length - this.additionalLength)) this.progressElement.value = 1;
+	this.progressElement.max = this.length;
+	if(!this.length) this.progressElement.value = 1;
 	else this.progressElement.value = this.progress;
 }
 
@@ -109,9 +105,10 @@ document.body.addEventListener('click', function(e) {
 	}
 	
 	if(e.target.classList.contains('deletebox')) liel = e.target.parentNode;
-	if(liel) 
+	if(liel) {
 		var s = liel.closest('SECTION')
 		if(!s) s = liel.closest('ARTICLE');
 		s.taskgroup.deleteTaskObj(liel.task);
 		liel.hidden = true;
+	}
 });
