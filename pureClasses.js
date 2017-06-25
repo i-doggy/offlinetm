80860f002ff7558c902272d099fe31543ce76453
i-doggy <kojlambus@gmail.com>
2017-06-25 23:59:19 +0500

    initial

---
+++ pureClasses.js
@@ -0,0 +1,95 @@
+﻿function Task(name, isAdditional, done) {
+	if(name) this.name = name;
+	else throw Error('Syntax Error: name is required');
+	
+	this.onchange = [];
+	
+	Object.defineProperty(this, 'done', { set: setBoolProp(this, '_isDone', 'onchange'), get: getProp(this, '_isDone')});
+	this._isDone = false;
+	setBoolProp(this, '_isDone', 'onchange')(done);
+	
+	this.onstateChange = [];
+	Object.defineProperty(this, 'isAdditional', { set: setBoolProp(this, '_isAdditional', 'onstateChange'), get: getProp(this, '_isAdditional')});
+	this.isAdditional = false;
+	if(isAdditional) this.isAdditional = true;
+	
+	Object.defineProperty(this, 'progress', { get: function() { if(this.isAdditional) return 0; else return Number(this.done); }});
+	return this;
+}
+
+Task.prototype.toggleDone = function() {
+	if(this.done) this.done = false;
+	else this.done = true;
+};
+
+function TaskGroup(name) {
+	if(name) this.name = name;
+	else throw Error('Syntax Error: name is required');
+	
+	this.onchange = [];
+	this.onstateChange = [];
+	
+	Object.defineProperty(this, 'progress', { get: this.getProgress });
+	Object.defineProperty(this, 'done', { get: this.getDone });
+	Object.defineProperty(this, 'length', { get: function() { return this.child.length + this.child.reduce(function(p, e) { if(e.length) return p + e.length - 1; else return p; }, 0); }});
+	
+	this.additionalLength = 0;
+	this.child = [].slice.call(arguments, 1).map(this.addTask, this);
+}
+
+TaskGroup.prototype.addTask = function (t) {
+		t.onchange.push(this._ChangeHandler(this));
+		t.onstateChange.push(this._StateChangeHandler(this));
+		if(t.isAdditional) this.additionalLength++
+		if(t.additionalLength) this.additionalLength = this.additionalLength ? this.additionalLength - 1 : 0 + t.additionalLength;
+		return t;
+}
+TaskGroup.prototype.getProgress = function() { 
+	return this.child.reduce(function(p, e) { 
+		return p + e.progress;
+	}, 0);
+}
+TaskGroup.prototype.getDone = function() { 
+	return this.progress == this.length - this.additionalLength; 
+}
+TaskGroup.prototype._ChangeHandler = function(obj) { return function(i) { obj.onchange.forEach(function(e) { e(i); }); }};
+TaskGroup.prototype._StateChangeHandler = function(obj) { return function(state) { 
+	if(state) obj.additionalLength++;
+	else obj.additionalLength--;
+	obj.onstateChange.forEach(function(e) { e(state); });
+}};
+TaskGroup.prototype.deleteTask = function(index) {
+	if(!index.inRange(-(this.child.length), this.child.length)) throw Error('Out of range');
+	findItemAndDelete(this.child[index].onchange, this._ChangeHandler(this));
+	findItemAndDelete(this.child[index].onstateChange, this._StateChangeHandler(this));
+	return this.child.splice(index, 1)[0];
+}
+TaskGroup.prototype.deleteTaskObj = function(task) {
+	var i = this.child.findIndex(function(e) { return e == task; });
+	this.deleteTask(i);
+}
+
+function setBoolProp(obj, propname, eventName) {
+	return function(value) {
+		var b = obj[propname] != Boolean(value)
+		obj[propname] = Boolean(value);
+		if(eventName)
+			if(b) 
+				obj[eventName].forEach(function(e) { e(Boolean(value)); });
+	};
+}
+
+function getProp(obj, propname) {
+	return function() { 
+		return obj[propname]
+	}
+};
+
+function findItemAndDelete(array, item) {
+	array.splice(array.findIndex(function(e) { return e == item; }), 1);
+}
+
+Number.prototype.inRange = function(start, end) {
+	if(this > start && this < end) return true;
+	else return false;
+}
---
+++ script.js
@@ -0,0 +1,117 @@
+﻿//CHERNOVIK
+function measureTotal(form) {
+	return form.length;
+}
+function measureChecked(form) {
+	var total = [].reduce.call(form, function(p, e) { return p + e.checked; }, 0);
+	return total
+}
+
+function RealTask(name, isAdditional, isDone, elem) {
+	Task.apply(this, [].slice.call(arguments, 0, 3));
+	if(elem) {
+		if(elem.tagName.toUpperCase() == 'LI') {
+			if(elem.children.length == 0)
+				this.element = elem;
+			else throw Error('Syntax Error: elem has children');
+		} else throw Error('Syntax Error: elem is not li element');
+	} else {
+		var el = document.createElement('LI')
+		
+		//<div class='checkbox'><span>&#x2713;<span></div>
+		var checkbox = document.createElement('DIV');
+		checkbox.classList.add('checkbox');
+		var check = document.createElement('SPAN');
+		check.innerText = '✓'
+		checkbox.append(check);
+		el.append(checkbox);
+		
+		var span = document.createElement('SPAN');
+		span.innerText = name;
+		span.contentEditable = "true";
+		el.append(span);
+		
+		//<div class='deletepoint'>&#x274C;</div>
+		var deletebox = document.createElement('DIV');
+		deletebox.innerText = '❌';
+		deletebox.classList.add('deletebox');
+		el.append(deletebox);
+		
+		el.task = this;
+		this.element = el;
+	}
+	if(this.isAdditional) this.element.classList.add('additional');
+	this.element.classList.add('task');
+	if(this.done) this.element.classList.add('checked');
+	return this;
+}
+
+RealTask.prototype = Object.create(Task.prototype);
+
+function RealTaskGroup(isList, name /*,tasks...*/) {
+	TaskGroup.apply(this, [].slice.call(arguments, 1));
+	
+	this._createElement(isList);
+	
+	this.renderProgress();
+	
+	return this;
+}
+
+RealTaskGroup.prototype = Object.create(TaskGroup.prototype);
+RealTaskGroup.prototype._createElement = function(isList) {
+	var container = document.createElement(isList ? 'ARTICLE' : 'SECTION');
+	var header = document.createElement('H1');
+	header.innerText = this.name;
+	container.append(header);
+	this.progressElement = document.createElement('PROGRESS');
+	this.progressElement.value = 0;
+	
+	container.append(this.progressElement);
+	var ul = document.createElement('UL');
+	this.child.forEach(function(e) { 
+		if(e.element.tagName == 'LI') ul.append(e.element);
+		else {
+			var li = document.createElement('LI');
+			li.append(e.element);
+			ul.append(li)
+		}});
+	container.append(ul);
+	container.taskgroup = this;
+	this.element = container;
+}
+
+RealTaskGroup.prototype._ChangeHandler = function(obj) { 
+	return function(i) { 
+		obj.renderProgress();
+		obj.onchange.forEach(function(e) { e(i); }); 
+	}
+};
+RealTaskGroup.prototype.deleteTaskObj = function(task) {
+	var res = TaskGroup.prototype.deleteTaskObj.call(this, task);
+	this.renderProgress();
+	return res;
+}
+RealTaskGroup.prototype.renderProgress = function() { 
+	this.progressElement.max = this.length - this.additionalLength;
+	if(!(this.length - this.additionalLength)) this.progressElement.value = 1;
+	else this.progressElement.value = this.progress;
+}
+
+document.body.addEventListener('click', function(e) {
+	var liel = false;
+	if(e.target.classList.contains('checkbox')) liel = e.target.parentNode;
+	if(e.target.parentNode.classList.contains('checkbox'))  liel = e.target.parentNode.parentNode;
+	if(liel) {
+		liel.classList.toggle('checked');
+		liel.task.toggleDone();
+		return;
+	}
+	
+	if(e.target.classList.contains('deletebox')) liel = e.target.parentNode;
+	if(liel) 
+		var s = liel.closest('SECTION')
+		if(!s) s = liel.closest('ARTICLE');
+		s.taskgroup.deleteTaskObj(liel.task);
+		liel.hidden = true;
+});
